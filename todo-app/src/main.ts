83b2fadec1d726baac5421dd5ff5b7387d49a0d0
boshuua <ws369808@weston.ac.uk>
2025-10-21 15:39:46 +0100

    refactor: improve layout and styling for better responsiveness and clarity

--- todo-app/src/main.ts
+++ todo-app/src/main.ts
@@ -1,8 +1,6 @@
-(window as any)['global'] = window; // FIX: Polyfill for older libraries like dragula
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));  
+  .catch((err) => console.error(err));
